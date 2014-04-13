1663542a9a8477fbf3277fdd91b040d8a85d80e0
Lucas Moran <lucas.moran@snhu.edu>
2014-04-13 17:16:40 -0400

    adding in AbstractTeam and TeamFactory, along with the much-needed config and plugin ymls.

---
+++ src/main/java/implement/team/TeamFactory.java
@@ -0,0 +1,14 @@
+package implement.team;
+
+import framework.team.Teamable;
+
+/**
+ * User: InspiredIdealist
+ * Date: 4/13/2014
+ */
+public class TeamFactory {
+
+    public static Teamable MakeNewTeam(String teamName) {
+        return null;
+    }
+}
