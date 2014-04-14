21f9870aea076916eeb370a845167ece860d8417
Lucas Moran <lucas.moran@snhu.edu>
2014-04-14 18:59:01 -0400

    Forgot to add this to the last commit... Will not compile without it!

---
+++ src/main/java/framework/utility/Coordinate.scala
@@ -0,0 +1,10 @@
+package framework.utility
+
+import org.bukkit.Material
+import org.bukkit.util.Vector
+
+/**
+ * User: InspiredIdealist
+ * Date: 4/13/2014
+ */
+class Coordinate(val v: Vector, var m: Material)
